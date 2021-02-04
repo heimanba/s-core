8494c3f076fd47394f1c3d21c5b569163acbe34c
wb-shl391697 <wb-shl391697@alibaba-inc.com>
2021-02-04 15:50:25 +0800

    feat: 🎸 add component credentials code

--- packages/core/src/components/index.ts
+++ packages/core/src/components/index.ts
@@ -1,5 +1,6 @@
-import { S_ROOT_HOME_COMPONENT } from '../libs/common';
 import fs from 'fs-extra';
+import inquirer from 'inquirer';
+import { S_ROOT_HOME_COMPONENT } from '../libs/common';
 import {
   buildComponentInstance,
   downloadComponent,
@@ -7,6 +8,8 @@ import {
   IComponentPath,
   installDependency,
 } from './load.service';
+import { GetManager } from './utils/getAccess';
+import { AddManager } from './utils/addAccess';
 
 export interface IComponent {
   load: (name: string, provider: string) => Promise<any>;
@@ -26,7 +29,10 @@ export class Component {
    */
   async load(name: string, provider: string) {
     const baseArgs = this.getBaseArgs(name, provider);
-    const componentPaths: IComponentPath = await generateComponentPath(baseArgs, S_ROOT_HOME_COMPONENT);
+    const componentPaths: IComponentPath = await generateComponentPath(
+      baseArgs,
+      S_ROOT_HOME_COMPONENT,
+    );
     const { componentPath, lockPath } = componentPaths;
     // 通过是否存在 .s.lock文件来判断
     if (!fs.existsSync(lockPath)) {
@@ -34,5 +40,65 @@ export class Component {
       await installDependency(baseArgs.name, componentPaths);
     }
     return await buildComponentInstance(componentPath);
+  }
+  async credentials(inputs: any) {
+    if (inputs.Credentials && Object.keys(inputs.Credentials).length > 0) {
+      return inputs.Credentials;
+    }
+    const Provider = inputs.Project.Provider;
+    const configUserInput = {
+      Provider,
+    };
+
+    const getManager = new GetManager();
+    await getManager.initAccessData(configUserInput);
+    const providerMap: {
+      [key: string]: any;
+    } = await getManager.getUserSecretID(configUserInput);
+
+    // 选择
+    const selectObject = [];
+    Object.keys(providerMap).forEach((item) => {
+      const temp = {
+        name: item.startsWith('project')
+          ? `${item.replace('project.', 'project: ')}`
+          : `${item.replace(Provider + '.', Provider + ': ')}`,
+        value: item,
+      };
+      if (Provider) {
+        if (item.startsWith(Provider) || item.startsWith('project')) {
+          selectObject.push(temp);
+        }
+      } else {
+        selectObject.push(temp);
+      }
+    });
+
+    selectObject.push({ name: 'Create a new account', value: 'create' });
+    let access = '';
+    await inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'access',
+          message: 'Please select an access:',
+          choices: selectObject,
+        },
+      ])
+      .then((answers: any) => {
+        access = answers.access;
+      });
+    if (access === 'create') {
+      const addManager = new AddManager();
+      const result = await addManager.inputLengthZero(Provider);
+
+      // 2020-9-23 修复部署过程中增加密钥信息，无法存储到系统的bug
+      const inputProviderAlias = `${Provider}.${addManager.aliasName || 'default'}`;
+      addManager.inputFullData[inputProviderAlias] = result;
+      addManager.writeData(addManager.globalFilePath, addManager.inputFullData);
+
+      return result;
+    }
+    return providerMap[access];
   }
 }
