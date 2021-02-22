f732ff200450ade84a5d55d9cfcec170e08d2560
wb-shl391697 <wb-shl391697@alibaba-inc.com>
2021-02-22 19:59:01 +0800

    feat: 🎸 add unzip

--- packages/core/test/fixtures/zip/index.ts
+++ packages/core/test/fixtures/zip/index.ts
@@ -1,10 +1,24 @@
-import { zip } from '../../../src/common';
+import { zip, unzip } from '../../../src/common';
 
-zip({
-  codeUri: './demo',
-  include: ['../spinner.ts'],
-  exclude: ['a.md'],
-  // exclude: ['dir'],
-  // exclude: ['./demo/dir'],
-  outputFilePath: './zipdist',
-});
+class ZipDemo {
+  testZip() {
+    zip({
+      codeUri: './demo',
+      include: ['../spinner.ts'],
+      exclude: ['a.md'],
+      // exclude: ['dir'],
+      // exclude: ['./demo/dir'],
+      outputFilePath: './zipdist',
+    });
+  }
+  async testUnzip() {
+    await unzip('./zipdist/dome.zip', 'unzip-dist');
+    console.log('done!');
+  }
+}
+
+const demo = new ZipDemo();
+
+// demo.testZip()
+
+demo.testUnzip();
